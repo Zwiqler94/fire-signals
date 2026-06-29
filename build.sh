a5871bb9d63dfc12c66af2605a4341a4bb00a078
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2026-06-29 18:56:46 -0400

    chore: update workflow and build scripts for improved npm publishing

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 SHORT_SHA=$(git rev-parse --short $GITHUB_SHA)
 TAG_TEST="^refs/tags/.+$"
 LATEST_TEST="^[^-]*$"
+ARTIFACT_REGISTRY="https://us-central1-npm.pkg.dev/jlz-portfolio/angular-libraries/"
 
 if [[ $GITHUB_REF =~ $TAG_TEST ]]; then
     OVERRIDE_VERSION=${GITHUB_REF/refs\/tags\//}
@@ -17,5 +18,5 @@ fi;
 npm --no-git-tag-version --allow-same-version -f version $OVERRIDE_VERSION
 
 npm run build &&
-    echo "npm publish . --tag $NPM_TAG" > ./dist/publish.sh &&
+    printf "npm publish . --registry %s --@zwiqler94:registry=%s --tag %s\n" "$ARTIFACT_REGISTRY" "$ARTIFACT_REGISTRY" "$NPM_TAG" > ./dist/publish.sh &&
     chmod +x ./dist/publish.sh
